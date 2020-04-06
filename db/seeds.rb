e53585370c7e60ac5473c85987851a53f9317ef1
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-04-06 13:35:45 +0400

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ Article.destroy_all
 puts "databases dropped!"
 
 puts "Creating articles..."
-navalo = Article.new(name:"Port Navalo Marine", categorie: "Sacs", price: 55)
+navalo = Article.new(name:"Port Navalo", categorie: "Sacs", price: 55, color: "Marine", description: "C'est la pièce phare de la collection Eté 2020 ! Porté épaule, ce sac assez grand et organisé pour y ranger nos essentiels facilement est imaginé, conçu et entièrement cousu en Bretagne. Ses petits boutons dorés et sa bandoulière unique et inédite complètent son look rétro et chic.")

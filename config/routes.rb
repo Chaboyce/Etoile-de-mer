7f1ec53355ad08a4bdc64dc8254d7cc393942ec2
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-04-08 20:46:35 +0400

    concept contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'concept', to: 'pages#concept'
+   get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   resources :categories

aa79bd1a319c6f1ab8d03d4d48b29c42809178cd
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-04-30 11:38:36 +0400

    remove contact migration

--- db/migrate/20200409114326_add_details_to_contacts.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToContacts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :contacts, :name, :string
-    add_column :contacts, :email, :string
-    add_column :contacts, :message, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -61,9 +61,6 @@ ActiveRecord::Schema.define(version: 2020_04_29_140032) do
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "email"
-    t.text "message"
   end
 
   create_table "users", force: :cascade do |t|

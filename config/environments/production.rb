3d889e3bfe5f18493767518dae00e7f728249ae4
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-04-30 19:03:35 +0400

    herku mailer set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,10 +111,10 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-config.action_mailer.default_url_options = { :host => 'https://omanette.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://omanette.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
+  config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',

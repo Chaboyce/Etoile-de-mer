d14201524c92d3ed56b2440db99bf35a80a12292
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-04-30 17:39:37 +0400

    prod set up mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,22 +111,19 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-config.action_mailer.default_url_options = { :host => 'omanette.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'omanette.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
-ActionMailer::Base.smtp_settings = {
-:user_name => ENV['SENDGRID_USERNAME'],
-:password => ENV['SENDGRID_PASSWORD'],
-:domain => 'herokuapp.com',
-:address => 'smtp.sendgrid.net',
-:port => 587,
-:authentication => :plain,
-:enable_starttls_auto => true
-
-
-}
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'omanette.com',
+  user_name:            ENV["GMAIL_EMAIL"],
+  password:             ENV["GMAIL_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 
 end

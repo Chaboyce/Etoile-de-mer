56b2d49a621e2bbb0ce5d11fb0d99e3df6522847
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-05-03 20:30:57 +0400

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,10 +117,10 @@ Rails.application.configure do
   #config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV["GMAIL_EMAIL"],
+  :password =>  ENV["GMAIL_PASSWORD"],
   :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true

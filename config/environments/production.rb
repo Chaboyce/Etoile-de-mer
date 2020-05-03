edcd817af07a5ace58dc4bfabdb357288ab5382d
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-05-03 19:54:41 +0400

    suppress stmp file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,15 +115,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = false
-#config.action_mailer.default :charset => "utf-8"
-#config.action_mailer.smtp_settings = {
-  #:user_name =>'apikey',
-  #:password => ENV['SENDGRID_API_KEY'],
-  #:domain => 'heroku.com',
-  #:address => 'smtp.sendgrid.net',
-  #:port => 587,
-  #:authentication => :plain,
-  #:enable_starttls_auto => true
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 #}
 
 

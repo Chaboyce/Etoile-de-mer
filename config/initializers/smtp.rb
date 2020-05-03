8abd1601f89da50028b2768b982b442587379f60
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-05-03 19:45:52 +0400

    changement port

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@
 
 ActionMailer::Base.smtp_settings = {
   address: 'heroku.com',
-  port: 465,
+  port: 587,
   domain: 'yourdomain.com',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],

762482ee3c75017fde930f1d690b582159db62b5
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-05-03 20:44:55 +0400

    emma receiver

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Message de Ômanette",
-      :to => "charlotte770@hotmail.com",
+      :to => "emmadeblignieres@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end

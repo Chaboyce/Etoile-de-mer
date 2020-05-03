c2d17e57e59c961216bc78d049fd0f2af646a2ca
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-05-03 19:23:35 +0400

    password

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,12 +1,12 @@
 # config/initializers/smtp.rb
-  ActionMailer::Base.smtp_settings = {
-  domain: 'heroku.com',
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  password:       ENV['SENDGRID_API_KEY']
+
+
+ActionMailer::Base.smtp_settings = {
+  address: 'heroku.com',
+  port: 587,
+  domain: 'yourdomain.com',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
 }
-
-
-

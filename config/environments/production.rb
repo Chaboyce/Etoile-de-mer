d165698322f262810eb88da939e2ca07dba9927f
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-07-25 16:06:21 +0200

    enlever le s de http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://www.omanette.com/" }
+  config.action_mailer.default_url_options = { host: "http://www.omanette.com/" }
 
 
 
